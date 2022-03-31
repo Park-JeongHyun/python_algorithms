03951f861419760cac7b7d654f1c44983e76a5d0
박정현 <benido124@gmail.com>
2022-03-31 23:06:50 +0900

    2022-03-31 study

---
+++ 2022-03/2022-03-31.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+
+n = int(input())
+a = [0]
+for _ in range(n):
+    a.append(int(input()))
+
+dp = [0]
+dp.append(a[1])
+if n > 1:
+    dp.append(a[1] + a[2])
+
+for i in range(3, n+1):
+    dp.append(max(dp[i-1], dp[i-3]+a[i-1]+a[i], dp[i-2]+a[i]))
+
+print(max(dp))
