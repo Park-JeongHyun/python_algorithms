026a87fe1567b4f80bef981bd3a6a55be6196ccc
PJH <pjh@PJHs-MacBook-Pro.local>
2022-04-19 13:49:14 +0900

    2022-04-19 study 문제 11866 요세푸스 문제0

---
+++ 2022-04/2022-04-19.py
@@ -0,0 +1,18 @@
+# 문제 11866 요세푸스 문제 0
+
+from collections import deque
+n, k = map(int, input().split())
+q = deque()
+
+for i in range(1, n+1):
+    q.append(i)
+
+print('<', end='')
+while q:
+    for i in range(k-1):
+        q.append(q[0])
+        q.popleft()
+    print(q.popleft(), end='')
+    if q:
+        print(', ', end='')
+print('>')
