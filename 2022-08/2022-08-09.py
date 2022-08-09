049d83aa0e37364810e6e8693ed55c83701a6e26
박정현 <benido124@gmail.com>
2022-08-09 22:28:44 +0900

    2022-08-09 study 문제 1145 적어도 대부분의 배수

---
+++ 2022-08/2022-08-09.py
@@ -0,0 +1,14 @@
+# 문제 1145 적어도 대부분의 배수
+
+a = list(map(int, input().split()))
+
+n = min(a)
+while True:
+    cnt = 0
+    for i in range(5):
+        if n % a[i] == 0:
+            cnt += 1
+    if cnt > 2:
+        print(n)
+        break
+    n += 1
