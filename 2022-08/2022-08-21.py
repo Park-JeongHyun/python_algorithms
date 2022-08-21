a25b492e4c157772a562ce983e1407edec4a7509
박정현 <benido124@gmail.com>
2022-08-21 22:45:14 +0900

    2022-08-21 study 문제 5532 방학숙제

---
+++ 2022-08/2022-08-21.py
@@ -0,0 +1,15 @@
+# 문제 5532 방학 숙제
+
+L = int(input())
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+cnt = 0
+while A > 0 or B > 0:
+    A -= C
+    B -= D
+    cnt += 1
+
+print(L - cnt)
