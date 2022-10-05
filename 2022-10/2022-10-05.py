cf2c9546fa493f519e503d3cf950f83920c2adb3
박정현 <benido124@gmail.com>
2022-10-05 23:33:57 +0900

    2022-10-05 study 문제 1453 피시방 알바

---
+++ 2022-10/2022-10-05.py
@@ -0,0 +1,15 @@
+# 문제 피시방 알바
+
+n = int(input())
+
+a = []
+cnt = 0
+b = list(map(int, input().split()))
+
+for i in b:
+    if i not in a:
+        a.append(i)
+    else:
+        cnt += 1
+
+print(cnt)
